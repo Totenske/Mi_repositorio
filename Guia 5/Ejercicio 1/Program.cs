b85f081226c0847c294c580b8bd0c2fad707e9fb
Totenske <AUGUSUTO_CASARETTO@HOTMAIL.COM>
2024-04-30 21:24:02 -0300

    Ejercicio 3 terminado

---
+++ Guia 5/Ejercicio 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            string nombre = "xxxx";
+            Console.WriteLine("El nombre es {0}", nombre);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Guia 5/ejercicio 3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lista,mayor_nota=0,nota,menor_nota=10,nro_libreta,menor_lib=0,mayor_lib=0;
+            string nombre, nombremayor="xxxx",nombremenor="yyyy";
+            Console.WriteLine("Introduzca el numero de almunos");
+            lista = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < lista; i++)
+            {
+                Console.WriteLine("ingrese el nro de libreta, nombre del alumno y su nota");
+                nro_libreta = Convert.ToInt32(Console.ReadLine());
+                nombre = Console.ReadLine();
+                nota = Convert.ToInt32(Console.ReadLine());
+                if (nota > mayor_nota)
+                {
+                    mayor_nota = nota;
+                    nombremayor = nombre;
+                    mayor_lib = nro_libreta;
+                }
+                if (nota < menor_nota)
+                {
+                    nombremenor = nombre;
+                    menor_nota = nota;
+                    menor_lib = nro_libreta;
+                }
+            }
+            Console.WriteLine("La mayor nota es {0}, con nombre {1} y libreta {2}",mayor_nota,nombremayor,mayor_lib);
+            Console.WriteLine("La menor nota es {0}, con nombre {1} y libreta {2}",menor_nota,nombremenor,menor_lib);
+            Console.ReadKey();
+        }
+    }
+}
