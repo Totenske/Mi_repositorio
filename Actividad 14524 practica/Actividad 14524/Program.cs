33b3351454a88c77b5d4aadfe3b034f1ba6c6f26
Casaretto <60686730+Totenske@users.noreply.github.com>
2024-05-14 20:14:21 -0300

    Actividades del 14524

---
+++ Actividad 14524 practica/Actividad 14524/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_14524
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, contador=0;
+            Console.WriteLine("ingrese el numero a verificar");
+            numero = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= numero; i++) {
+            if (numero % i == 0) {
+                contador++;
+                }
+            }
+            if (contador <= 2) {
+                Console.WriteLine("Es primo");
+            }
+            else { Console.WriteLine("No es primo"); }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Actividad 14524 practica/Actividad 2 14524/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2_14524
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
