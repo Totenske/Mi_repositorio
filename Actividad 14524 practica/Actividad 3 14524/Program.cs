e827ed40ca9e9b04db1d9c5672c6a2a2112ae92b
Totenske <augusto_casaretto@hotmail.com>
2024-05-14 21:42:35 -0300

    Actividad 3

---
+++ Actividad 14524 practica/Actividad 3 14524/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_3_14524
+{
+    internal class Program
+    {
+        public static void MostarMenu() 
+        {
+            Console.WriteLine("1 - Verificar Acceso");
+            Console.WriteLine("2 - Imprimir Recaudación");
+            Console.WriteLine("3 - Mostrar Cantidad de Accesos");
+            Console.WriteLine("Otros salir");
+        }
+        public static void VerificarAcceso() 
+        { 
+        
+        }
+        public static void imprimirRecaudacion() {
+        
+        }
+        public static void MostrarAccesos() 
+        { 
+        
+        }
+        static void Main(string[] args)
+        {
+            int opcion;
+            MostarMenu();
+            opcion=Convert.ToInt32(Console.ReadLine());
+            while (opcion >= 1 && opcion <= 3)
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        {
+                            VerificarAcceso();
+                            break;
+                        }
+                    case 2: {
+                            imprimirRecaudacion();
+                            break;
+                            }
+
+                    case 3: {
+                            MostrarAccesos();
+                            break;
+                            }
+                }
+                MostarMenu();
+                opcion = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Bye Bye");
+            Console.ReadKey();
+
+        }
+    }
+}
