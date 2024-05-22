64fdde9ce2ecbbae5ec795d2b8a3152961e4d40f
Totenske <augusto_casaretto@hotmail.com>
2024-05-21 21:39:00 -0300

    Avance actividad 3

--- Actividad 14524 practica/Actividad 3 14524/Program.cs
+++ Actividad 14524 practica/Actividad 3 14524/Program.cs
@@ -8,26 +8,68 @@ namespace Actividad_3_14524
 {
     internal class Program
     {
+        static int Recaudacion = 0;
+        #region Menu
         public static void MostarMenu() 
         {
+            Console.WriteLine("<------------------------------->");
             Console.WriteLine("1 - Verificar Acceso");
             Console.WriteLine("2 - Imprimir Recaudación");
             Console.WriteLine("3 - Mostrar Cantidad de Accesos");
             Console.WriteLine("Otros salir");
+            Console.WriteLine("<------------------------------->");
         }
+        #endregion
+        #region metodos
         public static void VerificarAcceso() 
         { 
-            Console.WriteLine("V");
+            Console.WriteLine("Ingrese 1 si tiene ticket o 0 de lo contrario");
+            int opcion2 = Convert.ToInt32(Console.ReadLine());
+            switch (opcion2)
+            {
+                case 0:
+                    {
+                        generar_el_ticket();
+                        break;
+                    }
+                case 1:
+                    {
+                        Console.WriteLine("Siga a la otra casilla");
+                        break;
+                    }
+            }
+        }
+        public static void generar_el_ticket() {
+            Console.WriteLine("Ingrese el vehiculo con el que entro");
+            Console.WriteLine("0 - Sin Vehiculo \n 1 - Moto \n 2 - Auto \n 3 - Camioneta \n 4 - Bugy \n 5 - Vehículo Nautico");
+            int Opcion = Convert.ToInt32(Console.ReadLine());
+            if (Opcion>0 && Opcion<5) {
+            switch (Opcion)
+                {
+                    case 0: { 
+                        
+                        }
+
+                        case
+
+                }
+
+
+            }
         }
         public static void imprimirRecaudacion() {
             Console.WriteLine("R");
         }
+        public static void calcularValor() { }
         public static void MostrarAccesos() 
         {
             Console.WriteLine("A");
         }
+        #endregion
+        #region Main
         static void Main(string[] args)
         {
+            int Vehiculos = 0;
             int opcion;
             MostarMenu();
             opcion=Convert.ToInt32(Console.ReadLine());
@@ -38,6 +80,7 @@ namespace Actividad_3_14524
                     case 1:
                         {
                             VerificarAcceso();
+                            Vehiculos++;
                             break;
                         }
                     case 2: {
@@ -47,6 +90,7 @@ namespace Actividad_3_14524
 
                     case 3: {
                             MostrarAccesos();
+                            Console.WriteLine(Vehiculos);
                             break;
                             }
                 }
@@ -57,5 +101,6 @@ namespace Actividad_3_14524
             Console.ReadKey();
 
         }
+        #endregion
     }
 }
