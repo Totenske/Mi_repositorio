506c8c8715a3c78c935a8dc8431423e7eaa86231
Totenske <augusto_casaretto@hotmail.com>
2024-05-28 20:24:21 -0300

    Cambiando de actividad

--- Actividad 28524/Raton.cs
+++ Actividad 28524/Raton.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Actividad_28524
 {
-    internal class Class1
+    public class Raton
     {
+        public bool TieneHambre;
+        public bool Vivo;
+        public void Comer(ref int queso)
+        {
+
+            if (Vivo == true)
+            {
+                if (queso >= 2)
+                {
+                    TieneHambre = true;
+                    queso -= 2;
+                }
+                else { if (queso <= 1) 
+                    {
+                      
+                    } 
+                }
+            }
+
+        }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_28524
+{
+    class gato
+    {
+        public bool TieneHambre;
+        public string Duenio;
+        public void Cazar(Raton r) {
+            if (r.Vivo && TieneHambre) {
+                r.Vivo = false;
+                TieneHambre = false;
+            }
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int queso = 50;
+            gato tom = new gato();
+
+
+            Raton Jerry = new Raton;
+            Jerry.Vivo = true;
+        }
+    }
+}
