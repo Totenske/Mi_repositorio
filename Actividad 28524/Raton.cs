4d5dc0a7314ee3a13cc90a88503855e20c35b7a6
Totenske <augusto_casaretto@hotmail.com>
2024-05-28 19:53:04 -0300

    Actividad del gato

---
+++ Actividad 28524/Raton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_28524
+{
+    internal class Class1
+    {
+    }
+}
